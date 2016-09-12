096f82df8fdd2cbdfda9cdf86817f87227664fdb
van thien <vanthien.nguyen@ved.com.vn>
2016-09-12 09:44:43 +0700

    Add APP_KEY, update installation guide

--- README.md
+++ README.md
@@ -4,7 +4,11 @@ Web app for managing kindergarten
 # Installation Guide
 - Copy .env.example to .env
 - Change database name, username, password in .env
-- Run: composer install
+- Run: 
+	composer install
+	php artisan vendor:publish --provider="Pingpong\Admin\AdminServiceProvider"
+	php artisan admin:install
+	php artisan db:seed
 - Set up virtual host local.dekidz.com
 
 # User Guide
--- sources/dekidz/database/seeds/RoleAndUserSeeder.php
+++ sources/dekidz/database/seeds/RoleAndUserSeeder.php
@@ -11,13 +11,7 @@ class RoleAndUserSeeder extends Seeder
      */
     public function run()
     {
-        $role = Pingpong\Trusty\Role::create([
-            'name' => 'Admin',
-            'slug' => 'admin',  // MUST USE admin, DO NOT CHANGE!
-            'description' => 'The one who manage the site',
-            'created_at' => new \Carbon\Carbon(),
-            'updated_at' => new \Carbon\Carbon(),
-        ]);
+        $role = Pingpong\Trusty\Role::where('slug', '=', 'admin')->first();
 
         $user = Pingpong\Admin\Entities\User::create([
             'name' => 'admin',
