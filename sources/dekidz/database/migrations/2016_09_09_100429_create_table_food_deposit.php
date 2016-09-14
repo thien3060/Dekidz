6e1ad86649678a6ad7826e0427db79aa4df1a8f2
Đoàn Xuân Thu <xuanthu@siliconstraits.vn>
2016-09-14 09:57:01 +0700

    change food deposit

--- sources/dekidz/database/migrations/2016_09_09_100429_create_table_food_deposit.php
+++ sources/dekidz/database/migrations/2016_09_09_100429_create_table_food_deposit.php
@@ -13,7 +13,7 @@ class CreateTableFoodDeposit extends Migration
     public function up()
     {
         Schema::create('food_deposit', function (Blueprint $table) {
-            $table->increments('id');
+            $table->increments('id', true);
             $table->string('type', 10);
             $table->string('name');
             $table->date('input_date');
