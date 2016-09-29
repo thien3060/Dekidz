87fafefe562ed42572ea4bb893ec511b30b1c473
van thien <vanthien.nguyen@ved.com.vn>
2016-09-29 10:50:03 +0700

    code structure

---
+++ sources/dekidz/app/Repositories/Eloquent/EloquentStudentRepository.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Repositories\Eloquent;
+
+use App\Repositories\Contracts\StudentRepository;
+
+class EloquentStudentRepository implements StudentRepository
+{
+    public function perPage()
+    {
+        // TODO: Implement perPage() method.
+    }
+
+    public function allOrSearch($searchQuery = null)
+    {
+        // TODO: Implement allOrSearch() method.
+    }
+
+    public function getAll()
+    {
+        // TODO: Implement getAll() method.
+    }
+
+    public function search($searchQuery)
+    {
+        // TODO: Implement search() method.
+    }
+
+    public function findById($id)
+    {
+        // TODO: Implement findById() method.
+    }
+
+    public function findBy($key, $value, $operator = '=')
+    {
+        // TODO: Implement findBy() method.
+    }
+
+    public function delete($id)
+    {
+        // TODO: Implement delete() method.
+    }
+
+    public function create(array $data)
+    {
+        // TODO: Implement create() method.
+    }
+
+    public function getStudent()
+    {
+        // TODO: Implement getStudent() method.
+    }
+
+}
