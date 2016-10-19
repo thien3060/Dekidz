ebdc3c74bd5b5c5582c0310edb3f2afdf5e38e87
Đoàn Xuân Thu <xuanthu@siliconstraits.vn>
2016-10-19 22:31:55 +0700

    Update index page for food, foodset, dish

--- sources/dekidz/resources/views/admin/pages/dishes/index.blade.php
+++ sources/dekidz/resources/views/admin/pages/dishes/index.blade.php
@@ -17,19 +17,19 @@
         <th>Prepare Description</th>
         <th>Cooking Description</th>
         <th>Chef</th>
-        <th>Protid DV</th>
-        <th>Protid TV</th>
-        <th>Lipip DV</th>
-        <th>Lipid TV</th>
-        <th>Glucid</th>
-        <th>Calo</th>
-        <th>Canxi</th>
-        <th>Iron</th>
-        <th>Vatamin A</th>
-        <th>Vitamin B1</th>
-        <th>Vatamin B2</th>
-        <th>Vatamin PP</th>
-        <th>Vatamin C</th>
+        {{--<th>Protid DV</th>--}}
+        {{--<th>Protid TV</th>--}}
+        {{--<th>Lipip DV</th>--}}
+        {{--<th>Lipid TV</th>--}}
+        {{--<th>Glucid</th>--}}
+        {{--<th>Calo</th>--}}
+        {{--<th>Canxi</th>--}}
+        {{--<th>Iron</th>--}}
+        {{--<th>Vatamin A</th>--}}
+        {{--<th>Vitamin B1</th>--}}
+        {{--<th>Vatamin B2</th>--}}
+        {{--<th>Vatamin PP</th>--}}
+        {{--<th>Vatamin C</th>--}}
 
         <th class="text-center">Action</th>
         </thead>
@@ -42,20 +42,20 @@
                 <td>{!! $dish->preparing_description !!}</td>
                 <td>{!! $dish->cooking_description !!}</td>
                 <td>{!! $dish->chef !!}</td>
-                <td>{!! $dish->protid_dv !!}</td>
-                <td>{!! $dish->protid_tv !!}</td>
-                <td>{!! $dish->lipid_dv !!}</td>
-                <td>{!! $dish->lipid_tv !!}</td>
-                <td>{!! $dish->glucid !!}</td>
-                <td>{!! $dish->calo !!}</td>
-                <td>{!! $dish->canxi !!}</td>
-                <td>{!! $dish->photpho !!}</td>
-                <td>{!! $dish->iron !!}</td>
-                <td>{!! $dish->vitamina !!}</td>
-                <td>{!! $dish->vitaminb1 !!}</td>
-                <td>{!! $dish->vitaminb2 !!}</td>
-                <td>{!! $dish->vitaminpp !!}</td>
-                <td>{!! $dish->vitaminc !!}</td>
+                {{--<td>{!! $dish->protid_dv !!}</td>--}}
+                {{--<td>{!! $dish->protid_tv !!}</td>--}}
+                {{--<td>{!! $dish->lipid_dv !!}</td>--}}
+                {{--<td>{!! $dish->lipid_tv !!}</td>--}}
+                {{--<td>{!! $dish->glucid !!}</td>--}}
+                {{--<td>{!! $dish->calo !!}</td>--}}
+                {{--<td>{!! $dish->canxi !!}</td>--}}
+                {{--<td>{!! $dish->photpho !!}</td>--}}
+                {{--<td>{!! $dish->iron !!}</td>--}}
+                {{--<td>{!! $dish->vitamina !!}</td>--}}
+                {{--<td>{!! $dish->vitaminb1 !!}</td>--}}
+                {{--<td>{!! $dish->vitaminb2 !!}</td>--}}
+                {{--<td>{!! $dish->vitaminpp !!}</td>--}}
+                {{--<td>{!! $dish->vitaminc !!}</td>--}}
                 <td class="text-center">
                     <a href="{!! route('admin.dishes.edit', $dish->id) !!}">Edit</a>
                     &middot;
--- sources/dekidz/resources/views/admin/pages/foods/index.blade.php
+++ sources/dekidz/resources/views/admin/pages/foods/index.blade.php
@@ -19,24 +19,6 @@
         <th>Quantity</th>
         <th>Unit</th>
         <th>Price</th>
-        <th>Absorb</th>
-        <th>Digest</th>
-        <th>Calo</th>
-        <th>H2O</th>
-        <th>Protid</th>
-        <th>Lipid</th>
-        <th>Glucid</th>
-        <th>Cellulose</th>
-        <th>Cholesterol</th>
-        <th>Calci</th>
-        <th>Photpho</th>
-        <th>Iron</th>
-        <th>Vitamin Caroten</th>
-        <th>Vatamin A</th>
-        <th>Vitamin B1</th>
-        <th>Vatamin B2</th>
-        <th>Vatamin PP</th>
-        <th>Vatamin C</th>
 
         <th class="text-center">Action</th>
         </thead>
@@ -50,25 +32,6 @@
                 <td>{!! $food->quantity !!}</td>
                 <td>{!! $food->unit !!}</td>
                 <td>{!! $food->price !!}</td>
-                <td>{!! $food->absorb !!}</td>
-                <td>{!! $food->digest !!}</td>
-                <td>{!! $food->calo !!}</td>
-                <td>{!! $food->h2o !!}</td>
-                <td>{!! $food->protid !!}</td>
-                <td>{!! $food->lipid !!}</td>
-                <td>{!! $food->glucid !!}</td>
-                <td>{!! $food->cellulose !!}</td>
-                <td>{!! $food->cholesterol !!}</td>
-                <td>{!! $food->calci !!}</td>
-                <td>{!! $food->photpho !!}</td>
-                <td>{!! $food->iron !!}</td>
-                <td>{!! $food->vitamin_caroten !!}</td>
-                <td>{!! $food->vitamina !!}</td>
-                <td>{!! $food->vitaminb1 !!}</td>
-                <td>{!! $food->vitaminb2 !!}</td>
-                <td>{!! $food->vitaminpp !!}</td>
-                <td>{!! $food->vitaminc !!}</td>
-                <td>{!! $food->vitaminc !!}</td>
                 <td class="text-center">
                     <a href="{!! route('admin.foods.edit', $food->id) !!}">Edit</a>
                     &middot;
