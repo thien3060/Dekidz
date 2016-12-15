20edc0d0e8f351f049a5a940d7fef7a55e4932b1
thien <thien3060@gmail.com>
2016-12-15 21:28:34 +0700

    stream video

---
+++ sources/dekidz/config/custom.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thien
+ * Date: 12/15/2016
+ * Time: 8:45 PM
+ */
+
+return [
+    'embedded_videos' => [
+        'camera_1' =>  [
+            'title' => 'Camera 1',
+            'url' => 'https://www.youtube.com/embed/BvTBuHDLYVY'
+        ],
+        'camera_2' => [
+            'title' => 'Camera 2',
+            'url' => 'https://www.youtube.com/embed/uaqKS1QNTNc'
+        ]
+    ]
+];
